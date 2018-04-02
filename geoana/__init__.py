654b8acc339259e07337674b8018ca25e0634eca
Lindsey Heagy <lindseyheagy@gmail.com>
2018-04-01 19:25:13 -0700

    Bump version: 0.0.1 → 0.0.2

--- geoana/__init__.py
+++ geoana/__init__.py
@@ -2,7 +2,7 @@ from . import earthquake
 from . import em
 from . import utils
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __author__ = 'SimPEG developers'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2017 SimPEG developers'
