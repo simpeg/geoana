0e41866c9179d61ab2ede7ecea1dfcdbae31c1e4
seogi_macbook <sgkang09@github.com>
2018-08-20 12:15:09 -0700

    Bump version: 0.0.4 → 0.0.5

--- geoana/__init__.py
+++ geoana/__init__.py
@@ -2,7 +2,7 @@ from . import earthquake
 from . import em
 from . import utils
 
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 __author__ = 'SimPEG developers'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2017-2018 SimPEG developers'
