fix sign convention for halfspace

geoana/em/fdem/halfspace.py
@@ -55,7 +55,7 @@ class MagneticDipoleHalfSpace(BaseMagneticDipole, BaseFDEM):
         IK2 = iv(2, alpha)*kv(2, alpha)
         if src_z != 0.0:
             em_z += src_z*2.0/(k**2*r**5)*(9-(9+9*1j*k*r-4*k**2*r**2-1j*k**3*r**3)*np.exp(-1j*k*r))
-            Hr = (-k**2/r)*(IK1 - IK2)
+            Hr = (k**2/r)*(IK1 - IK2)
             angle = np.arctan2(y, x)
             em_x += src_z*np.cos(angle)*Hr
             em_y += src_z*np.sin(angle)*Hr
@@ -67,13 +67,13 @@ class MagneticDipoleHalfSpace(BaseMagneticDipole, BaseFDEM):
             if src_x != 0.0:
                 em_x += src_x*(-1.0/r**3)*(y**2*phi + x**2*r*dphi_dr)
                 em_y += src_x*(1.0/r**3)*x*y*(phi - r*dphi_dr)
-                em_z += src_x*(k**2*x/r**2)*(IK1 - IK2)  # is this negative???
+                em_z -= src_x*(k**2*x/r**2)*(IK1 - IK2)
 
             # Y component of source
             if src_y != 0.0:
                 em_x += src_y*(1.0/r**3)*x*y*(phi - r*dphi_dr)
                 em_y += src_y*(-1.0/r**3)*(x**2*phi + y**2*r*dphi_dr)
-                em_z += src_y*(k**2*y/r**2)*(IK1 - IK2)  # is this negative???
+                em_z -= src_y*(k**2*y/r**2)*(IK1 - IK2)
 
         if field == "secondary":
             # subtract out primary field from above