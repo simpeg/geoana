d04c27857c739bd4b0cb3d3a167b733c3f7203e1
Joseph Capriotti <josephrcapriotti@gmail.com>
2020-11-19 16:07:38 -0800

    Bump version: 0.1.0 → 0.1.1

--- geoana/__init__.py
+++ geoana/__init__.py
@@ -2,7 +2,7 @@ from . import earthquake
 from . import em
 from . import utils
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __author__ = 'SimPEG developers'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2017-2018 SimPEG developers'
