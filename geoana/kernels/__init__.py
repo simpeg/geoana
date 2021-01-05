d36de8a384b5fb6a7e95d0038b62be084953c3e9
Seogi Kang <sgkang09@gmail.com>
2021-01-05 11:49:14 -0800

    bug fix for import error

--- geoana/kernels/__init__.py
+++ geoana/kernels/__init__.py
@@ -1,3 +1,4 @@
 """This module contains kernals for (semi-)analytic geophysical responses
 """
 from geoana.kernels.tranverse_electric_reflections import rTE_forward, rTE_gradient
+from . import _extensions
