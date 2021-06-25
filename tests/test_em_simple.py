3efcceec766e419f16e56d1a5a884eff786b039c
Joseph Capriotti <josephrcapriotti@gmail.com>
2021-06-24 17:42:02 -0700

    update tolerances

--- tests/test_em_simple.py
+++ tests/test_em_simple.py
@@ -52,7 +52,7 @@ class testExamples(unittest.TestCase):
         np.testing.assert_allclose(
             hz[-1],
             radius**2/(30*np.sqrt(np.pi))*(sigma*mu_0/times[-1])**(1.5),
-            rtol=1E-6
+            rtol=1E-4
         )
 
         bz = t_bz_loop(times, sigma=sigma, radius=radius)
@@ -65,7 +65,7 @@ class testExamples(unittest.TestCase):
         np.testing.assert_allclose(
             dhz_dt[-1],
             -radius**2/(20*np.sqrt(np.pi))*(sigma*mu_0/times[-1])**(1.5)/times[-1],
-            rtol=1E-6
+            rtol=1E-4
         )
 
         dbz_dt = t_bzdt_loop(times, sigma=sigma, radius=radius)
@@ -84,7 +84,7 @@ class testExamples(unittest.TestCase):
         np.testing.assert_allclose(
             h[-1, 0],
             offset/(128*np.pi)*(mu_0*sigma/times[-1])**2,
-            rtol=1E-5,
+            rtol=1E-3,
         )
         np.testing.assert_allclose(
             h[-1, 2],
