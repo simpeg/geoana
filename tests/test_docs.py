ef3317c2334fd91b3f416b5a42248ef7e72c5f68
Joseph Capriotti <josephrcapriotti@gmail.com>
2022-04-01 10:13:51 -0700

    Test new doc build testing, without running as subprocess

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -1,9 +1,6 @@
 import os
-import subprocess
 import unittest
-import platform
-
-use_shell = platform.system() == "Windows"
+from sphinx.application import Sphinx
 
 
 class TestDoc(unittest.TestCase):
@@ -14,16 +11,19 @@ class TestDoc(unittest.TestCase):
         return os.path.sep.join(dirname.split(os.path.sep)[:-1] + ["docs"])
 
     def test_html(self):
-        os.chdir(self.path_to_docs)
-
-        response = subprocess.run(["make", "html"], shell=use_shell)
-        self.assertTrue(response.returncode == 0)
+        src_dir = config_dir = self.path_to_docs
+        output_dir = os.path.sep.join([src_dir, "_build", "html"])
+        doctree_dir = os.path.sep.join([src_dir, "_build", "doctree"])
+        app = Sphinx(src_dir, config_dir, output_dir, doctree_dir, buildername="html", warningiserror=False)
+        app.build(force_all=True)
 
     def test_linkcheck(self):
-        os.chdir(self.path_to_docs)
+        src_dir = config_dir = self.path_to_docs
+        output_dir = os.path.sep.join([src_dir, "_build", "linkcheck"])
+        doctree_dir = os.path.sep.join([src_dir, "_build", "doctree"])
+        app = Sphinx(src_dir, config_dir, output_dir, doctree_dir, buildername="linkcheck", warningiserror=False)
+        app.build(force_all=True)
 
-        response = subprocess.run(["make", "linkcheck"], shell=use_shell)
-        self.assertTrue(response.returncode == 0)
 
 if __name__ == '__main__':
     unittest.main()
