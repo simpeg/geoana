379c745d4a5ca1b465b7d0bbd5616387d3c7cfc6
Joseph Capriotti <josephrcapriotti@gmail.com>
2022-03-31 18:04:18 -0700

    reset test_docs and remove codacy links

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -1,12 +1,6 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-
 import os
 import subprocess
 import unittest
-import platform
 
 
 class TestDoc(unittest.TestCase):
@@ -14,34 +8,19 @@ class TestDoc(unittest.TestCase):
     @property
     def path_to_docs(self):
         dirname, file_name = os.path.split(os.path.abspath(__file__))
-        return dirname.split(os.path.sep)[:-1] + ["docs"]
+        return os.path.sep.join(dirname.split(os.path.sep)[:-1] + ["docs"])
 
     def test_html(self):
-        wd = os.getcwd()
-        os.chdir(os.path.sep.join(self.path_to_docs))
+        os.chdir(self.path_to_docs)
 
-        if platform.system() != "Windows":
-            response = subprocess.run(["make", "html"])
-            self.assertTrue(response.returncode == 0)
-        else:
-            response = subprocess.call(["make", "html"], shell=True)
-            self.assertTrue(response == 0)
-
-        os.chdir(wd)
+        response = subprocess.run(["make", "html"])
+        self.assertTrue(response.returncode == 0)
 
     def test_linkcheck(self):
-        wd = os.getcwd()
-        os.chdir(os.path.sep.join(self.path_to_docs))
+        os.chdir(self.path_to_docs)
 
-        if platform.system() != "Windows":
-            response = subprocess.run(["make", "linkcheck"])
-            self.assertTrue(response.returncode == 0)
-        else:
-            response = subprocess.call(["make", "linkcheck"], shell=True)
-            self.assertTrue(response == 0)
-
-        os.chdir(wd)
-
+        response = subprocess.run(["make", "linkcheck"])
+        self.assertTrue(response.returncode == 0)
 
 if __name__ == '__main__':
     unittest.main()
