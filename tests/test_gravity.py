4d1c1dc0a04cd0bac00956cf0b5eefdde612d6d6
kaicglee <51426740+Syckdog@users.noreply.github.com>
2022-05-03 15:19:46 -0700

    created tests for gravitational field and gradient (not passing)

--- tests/test_gravity.py
+++ tests/test_gravity.py
@@ -5,6 +5,7 @@ from scipy.constants import G
 
 from geoana import gravity
 
+
 def U_from_PointMass(
     XYZ, loc, m
 ):
@@ -22,6 +23,42 @@ def U_from_PointMass(
     return u_g
 
 
+def g_from_PointMass(
+        XYZ, loc, m
+):
+
+    XYZ = discretize.utils.asArray_N_x_Dim(XYZ, 3)
+    # Check
+
+    dx = XYZ[:, 0] - loc[0]
+    dy = XYZ[:, 1] - loc[1]
+    dz = XYZ[:, 2] - loc[2]
+
+    r_vec = np.array([dx, dy, dz])
+    r = np.sqrt(dx ** 2. + dy ** 2. + dz ** 2.)
+
+    g_vec = (G * m * r_vec) / r
+    return g_vec
+
+
+def ggtens_from_PointMass(
+        XYZ, loc, m
+):
+
+    XYZ = discretize.utils.asArray_N_x_Dim(XYZ, 3)
+    # Check
+
+    dx = XYZ[:, 0] - loc[0]
+    dy = XYZ[:, 1] - loc[1]
+    dz = XYZ[:, 2] - loc[2]
+
+    r_vec = np.array([dx, dy, dz])
+    r = np.sqrt(dx ** 2. + dy ** 2. + dz ** 2.)
+
+    gg_tens = (G * m * np.eye) / r ** 3 + (3 * np.outer(r_vec, r_vec)) / r ** 5
+    return gg_tens
+
+
 class TestPointMass:
 
     def test_defaults(self):
@@ -32,7 +69,7 @@ class TestPointMass:
     def test_gravitational_potential(self):
         mass = 1
         pm = gravity.PointMass(
-            mass=mass,
+            mass=mass
         )
         x = np.linspace(-20., 20., 50)
         y = np.linspace(-30., 30., 50)
@@ -46,5 +83,41 @@ class TestPointMass:
         u = pm.gravitational_potential(xyz)
 
         assert np.all(utest == u)
+
+    def test_gravitational_field(self):
+        mass = 1
+        pm = gravity.PointMass(
+            mass=mass
+        )
+        x = np.linspace(-20., 20., 50)
+        y = np.linspace(-30., 30., 50)
+        z = np.linspace(-40., 40., 50)
+        xyz = discretize.utils.ndgrid([x, y, z])
+
+        gtest = g_from_PointMass(
+            xyz, pm.location, pm.mass
+        )
+
+        g = pm.gravitational_field(xyz)
+
+        assert np.all(gtest == g)
+
+    def test_gravitational_gradient(self):
+        mass = 1
+        pm = gravity.PointMass(
+            mass=mass
+        )
+        x = np.linspace(-20., 20., 50)
+        y = np.linspace(-30., 30., 50)
+        z = np.linspace(-40., 40., 50)
+        xyz = discretize.utils.ndgrid([x, y, z])
+
+        ggtenstest = ggtens_from_PointMass(
+            xyz, pm.location, pm.mass
+        )
+
+        ggtens = pm.gravitational_gradient(xyz)
+
+        assert np.all(ggtenstest == ggtens)
 
 
