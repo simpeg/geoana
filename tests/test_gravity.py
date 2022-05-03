71ea75406034fde94aa63e10d7b72d7072551dcc
kaicglee <51426740+Syckdog@users.noreply.github.com>
2022-05-03 15:10:01 -0700

    created test for gravitational potential

--- tests/test_gravity.py
+++ tests/test_gravity.py
@@ -1,8 +1,25 @@
 import pytest
+import discretize
 import numpy as np
 from scipy.constants import G
 
 from geoana import gravity
+
+def U_from_PointMass(
+    XYZ, loc, m
+):
+
+    XYZ = discretize.utils.asArray_N_x_Dim(XYZ, 3)
+    # Check
+
+    dx = XYZ[:, 0]-loc[0]
+    dy = XYZ[:, 1]-loc[1]
+    dz = XYZ[:, 2]-loc[2]
+
+    r = np.sqrt(dx**2. + dy**2. + dz**2.)
+
+    u_g = (G * m) / r
+    return u_g
 
 
 class TestPointMass:
@@ -10,6 +27,24 @@ class TestPointMass:
     def test_defaults(self):
         pm = gravity.PointMass()
         assert pm.mass == 1
-        assert np.allclose(pm.location, np.array([0, 0, 0]))
+        assert np.all(pm.location == np.r_[0., 0., 0.])
+
+    def test_gravitational_potential(self):
+        mass = 1
+        pm = gravity.PointMass(
+            mass=mass,
+        )
+        x = np.linspace(-20., 20., 50)
+        y = np.linspace(-30., 30., 50)
+        z = np.linspace(-40., 40., 50)
+        xyz = discretize.utils.ndgrid([x, y, z])
+
+        utest = U_from_PointMass(
+            xyz, pm.location, pm.mass
+        )
+
+        u = pm.gravitational_potential(xyz)
+
+        assert np.all(utest == u)
 
 
