e8ba84111f3f7b0131f08092375d99337950949c
kaicglee <51426740+Syckdog@users.noreply.github.com>
2022-05-03 14:17:41 -0700

    changed properties and added documentation

--- geoana/gravity.py
+++ geoana/gravity.py
@@ -5,18 +5,121 @@ from scipy.constants import G
 
 
 class PointMass:
+    """Gravity of a point mass.
 
-    """
-    Gravity due to a point mass
-    """
+        Parameters
+        ----------
+        mass : float, optional
+            Mass of the point particle in kg. Default is m = 1 kg
+        location : array_like, optional
+            Location of the point mass in 3D space. Default is (0,0,0)
+        """
 
-    mass = properties.Float(
-        "mass of the point particle (kg)", default=1.
-    )
+    def __init__(self, mass=1.0, location=np.r_[0., 0., 0.], **kwargs):
 
-    location = properties.Float(
-        "location of the point mass (m)", default=1
-    )
+        self.mass = mass
+        self.location = location
+        super().__init__(**kwargs)
+
+    @property
+    def mass(self):
+        """Mass of the point particle in kg
+
+        Returns
+        -------
+        float
+            Mass of the point particle in kg
+        """
+        return self._mass
+
+    @mass.setter
+    def sigma(self, value):
+
+        try:
+            value = float(value)
+        except:
+            raise TypeError(f"mass must be a number, got {type(value)}")
+
+        if value <= 0.0:
+            raise ValueError("mass must be greater than 0")
+
+        self._mass = value
+
+    @property
+    def location(self):
+        """Location of the point mass
+
+        Returns
+        -------
+        (3) numpy.ndarray of float
+            xyz point mass location
+        """
+        return self._location
+
+    @location.setter
+    def location(self, vec):
+
+        try:
+            vec = np.asarray(vec, dtype=float)
+        except:
+            raise TypeError(f"location must be array_like of float, got {type(vec)}")
+        vec = np.squeeze(vec)
+        if vec.shape != (3,):
+            raise ValueError(
+                f"location must be array_like with shape (3,), got {vec.shape}"
+            )
+
+        self._location = vec
+
+    def vector_distance(self, xyz):
+        r"""Vector distance to a set of gridded xyz locations.
+
+        Where :math:`\mathbf{p}` is the location of the source and :math:`\mathbf{q}`
+        is a point in 3D space, this method returns the vector distance:
+
+        .. math::
+            \mathbf{v} = \mathbf{q} - \mathbf{p}
+
+        for all locations :math:`\mathbf{q}` supplied in the inputed argument `xyz`.
+        For dipoles, :math:`\mathbf{p}` is the dipole location. For circular loops,
+        :math:`\mathbf{p}` defines the center location for the loop.
+
+        Parameters
+        ----------
+        xyz : (n, 3) numpy.ndarray
+            Gridded xyz locations
+
+        Returns
+        -------
+        (n, 3) numpy.ndarray
+            Vector distances in the x, y and z directions
+        """
+        return xyz - self.location
+
+    def distance(self, xyz):
+        r"""Scalar distance from dipole to a set of gridded xyz locations
+
+        Where :math:`\mathbf{p}` is the location of the source and :math:`\mathbf{q}`
+        is a point in 3D space, this method returns the scalar distance:
+
+        .. math::
+            d = \sqrt{(q_x - p_x)^2 + (q_y - p_y)^2 + (q_z - p_z)^2}
+
+        for all locations :math:`\mathbf{q}` supplied in the input argument `xyz`.
+        For dipoles, :math:`\mathbf{p}` is the dipole location. For circular loops,
+        :math:`\mathbf{p}` defines the center location for the loop.
+
+        Parameters
+        ----------
+        xyz : (n, 3) numpy.ndarray
+            Gridded xyz locations
+
+        Returns
+        -------
+        (n) numpy.ndarray
+            Scalar distances from dipole to xyz locations
+        """
+        return np.linalg.norm(xyz - self.location, axis=-1)
 
     def gravitational_potential(self, xyz):
         """
@@ -28,8 +131,7 @@ class PointMass:
             U(P) = \\gamma \\frac{m}{r}
 
         """
-        r_vec = xyz - self.location
-        r = np.linalg.norm(r_vec, axis=-1)
+        r = self.distance(xyz)
         u_g = (G * self.mass) / r
         return u_g
 
@@ -43,8 +145,8 @@ class PointMass:
             \\mathbf{g} = \\nabla U(P)
 
         """
-        r_vec = xyz - self.location
-        r = np.linalg.norm(r_vec, axis=-1)
+        r_vec = self.vector_distance(xyz)
+        r = self.distance(xyz)
         g_vec = (G * self.mass * r_vec) / r
         return g_vec
 
@@ -57,7 +159,7 @@ class PointMass:
             \\Gamma = \\nabla \\mathbf{g}
 
         """
-        r_vec = xyz - self.location
-        r = np.linalg.norm(r_vec, axis=-1)
+        r_vec = self.vector_distance(xyz)
+        r = self.distance(xyz)
         gg_tens = (G * self.mass * np.eye) / r ** 3 + (3 * np.outer(r_vec, r_vec)) / r ** 5
         return gg_tens
--- tests/test_gravity.py
+++ tests/test_gravity.py
@@ -1,9 +1,34 @@
-import unittest
+import pytest
 import numpy as np
-import properties
+import discretize
 from scipy.constants import G
 
 from geoana import gravity
 
-## class TestPointMass(unittest.TestCase):
 
+class TestPointMass:
+
+    def test_defaults(self):
+        pm = gravity.PointMass()
+        assert(pm.mass == 1)
+        assert(pm.location == 1)
+
+    def test_gravitational_potential(self):
+        mass = np.random.random_integers(1)
+        location = np.random.random_integers(1)
+        pm = gravity.PointMass(
+            mass=mass,
+            location=location
+        )
+        x = np.linspace(-20., 20., 50)
+        y = np.linspace(-30., 30., 50)
+        z = np.linspace(-40., 40., 50)
+        xyz = discretize.utils.ndgrid([x, y, z])
+
+        u = pm.gravitational_potential(xyz)
+
+        r_vec = xyz - location
+        r = np.linalg.norm(r_vec)
+        u_g = (G * mass) / location
+
+        self.assertEqual(u, u_g)
