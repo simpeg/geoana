619564f7328ba91f95bd49df088d408850bfe853
Joseph Capriotti <josephrcapriotti@gmail.com>
2022-05-11 11:59:33 -0700

    Bump version: 0.2.0 → 0.2.1

--- docs/conf.py
+++ docs/conf.py
@@ -88,9 +88,9 @@ author = u'SimPEG Team'
 # built documents.
 #
 # The short X.Y version.
-version = u'0.2.0'
+version = u'0.2.1'
 # The full version, including alpha/beta/rc tags.
-release = u'0.2.0'
+release = u'0.2.1'
 
 
 linkcheck_retries = 3
