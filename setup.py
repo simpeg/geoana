3d3e021a859670a675227aa274db6d158fbafe4d
Joseph Capriotti <josephrcapriotti@gmail.com>
2022-08-17 13:09:35 -0700

    Bump version: 0.2.3 → 0.3.0

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ with open('README.rst') as f:
 
 metadata = dict(
     name = 'geoana',
-    version = '0.2.3',
+    version = '0.3.0',
     python_requires=">=3.6",
     setup_requires=[
         "numpy>=1.8",
