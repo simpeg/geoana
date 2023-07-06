e81c6266dfb4256f7a4911c4bd97de64a58d3fda
Joseph Capriotti <josephrcapriotti@gmail.com>
2023-07-06 11:30:15 -0700

    Bump version: 0.4.0 → 0.4.1

--- docs/conf.py
+++ docs/conf.py
@@ -88,9 +88,9 @@ author = u'SimPEG Team'
 # built documents.
 #
 # The short X.Y version.
-version = u'0.4.0'
+version = u'0.4.1'
 # The full version, including alpha/beta/rc tags.
-release = u'0.4.0'
+release = u'0.4.1'
 
 linkcheck_retries = 3
 linkcheck_timeout = 500
--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ with open('README.rst') as f:
 
 metadata = dict(
     name = 'geoana',
-    version = '0.4.0',
+    version = '0.4.1',
     python_requires=">=3.6",
     setup_requires=[
         "numpy>=1.8",
