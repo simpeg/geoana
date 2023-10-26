925a3a4ddeede65ce8cd5e9e8f505a9b048c4c97
Joseph Capriotti <josephrcapriotti@gmail.com>
2023-10-26 17:14:15 -0600

    add optionally handles to the underlying c functions for prism kernels

--- geoana/kernels/__init__.py
+++ geoana/kernels/__init__.py
@@ -12,3 +12,61 @@ from geoana.kernels.potential_field_prism import (
     prism_fxxz,
     prism_fxyz,
 )
+
+try:
+    from numba.extending import get_cython_function_address
+    import ctypes
+
+    def __as_ctypes_func(module, function, argument_types):
+        func_address = get_cython_function_address(module, function)
+        func_type = ctypes.CFUNCTYPE(*argument_types)
+        return func_type(func_address)
+
+    c_prism = __as_ctypes_func(
+        'geoana.kernels._extensions.potential_field_prism',
+        'prism_f',
+        (ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double)
+    )
+    c_prism_fz = __as_ctypes_func(
+        'geoana.kernels._extensions.potential_field_prism',
+        'prism_fz',
+        (ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double)
+    )
+    c_prism_fzz = __as_ctypes_func(
+        'geoana.kernels._extensions.potential_field_prism',
+        'prism_fzz',
+        (ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double)
+    )
+    c_prism_fzx = __as_ctypes_func(
+        'geoana.kernels._extensions.potential_field_prism',
+        'prism_fzx',
+        (ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double)
+    )
+    c_prism_fzy = __as_ctypes_func(
+        'geoana.kernels._extensions.potential_field_prism',
+        'prism_fzy',
+        (ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double)
+    )
+    c_prism_fzzz = __as_ctypes_func(
+        'geoana.kernels._extensions.potential_field_prism',
+        'prism_fzzz',
+        (ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double)
+    )
+    c_prism_fxxy = __as_ctypes_func(
+        'geoana.kernels._extensions.potential_field_prism',
+        'prism_fxxy',
+        (ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double)
+    )
+    c_prism_fxxz = __as_ctypes_func(
+        'geoana.kernels._extensions.potential_field_prism',
+        'prism_fxxz',
+        (ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double)
+    )
+    c_prism_fxyz = __as_ctypes_func(
+        'geoana.kernels._extensions.potential_field_prism',
+        'prism_fxyz',
+        (ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double)
+    )
+
+except ImportError:
+    pass
