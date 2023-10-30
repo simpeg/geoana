a89f1ec26518771533e08d02d16279e88a62a6ce
Joseph Capriotti <josephrcapriotti@gmail.com>
2023-10-30 15:28:41 -0600

    pyproject.toml

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -1,6 +1,9 @@
 import os
 import unittest
 from sphinx.application import Sphinx
+import multiprocessing
+
+n_cpu = multiprocessing.cpu_count()
 
 
 class TestDoc(unittest.TestCase):
@@ -14,15 +17,15 @@ class TestDoc(unittest.TestCase):
         src_dir = config_dir = self.path_to_docs
         output_dir = os.path.sep.join([src_dir, "_build", "html"])
         doctree_dir = os.path.sep.join([src_dir, "_build", "doctree"])
-        app = Sphinx(src_dir, config_dir, output_dir, doctree_dir, buildername="html", warningiserror=False)
-        app.build(force_all=True)
+        app = Sphinx(src_dir, config_dir, output_dir, doctree_dir, buildername="html", warningiserror=False, parallel=n_cpu)
+        app.build()
 
     def test_linkcheck(self):
         src_dir = config_dir = self.path_to_docs
         output_dir = os.path.sep.join([src_dir, "_build", "linkcheck"])
         doctree_dir = os.path.sep.join([src_dir, "_build", "doctree"])
-        app = Sphinx(src_dir, config_dir, output_dir, doctree_dir, buildername="linkcheck", warningiserror=False)
-        app.build(force_all=True)
+        app = Sphinx(src_dir, config_dir, output_dir, doctree_dir, buildername="linkcheck", warningiserror=False, parallel=n_cpu)
+        app.build()
 
 
 if __name__ == '__main__':
